1c020f67929240cfb76ef198c681692408ffe12e
hubiqing01 <hubiqing01@58ganji.com>
2017-04-17 16:27:25 +0800

    4

--- server/inits/initDirectory.js
+++ server/inits/initDirectory.js
@@ -1,14 +1,13 @@
-  import mkdirp from 'mkdirp-promise';
-  import fsp from 'fs-promise';
   const dir = APP_CONFIG.uploadPath;
   //待初始化路径 
   let initDirs = [dir];
   export default {
       init(app, { debug }) {
           initDirs.forEach((dir) => {
-              const exists = fsp.existsSync(dir);
+              const ioHelper = appUtils.requireCommon('ioHelper');
+              const exists = ioHelper.existsSync(dir);
               if (!exists) {
-                  mkdirp(dir).then(() => {
+                  ioHelper.mkdir(dir).then(() => {
                       debug(`directory:${dir} OK.`);
                   });
               } else {

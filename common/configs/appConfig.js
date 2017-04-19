aced3826c2f033864ec824d03c02ad0d40cb8432
hubiqing01 <hubiqing01@58ganji.com>
2017-04-19 11:26:29 +0800

    localhost->127.0.0.1

--- common/configs/appConfig.js
+++ common/configs/appConfig.js
@@ -1,6 +1,6 @@
 import path from 'path';
 export default {
-    mongodb: 'mongodb://localhost:27017/koa2-base',
+    mongodb: 'mongodb://127.0.0.1:27017/koa2-base',
     apiPrefix: 'api',
     hostPrefix: '', //应用程序上线域名前缀
     staticPath: path.join(global.ROOT_PATH, '../koa2-base-fe/dist'),

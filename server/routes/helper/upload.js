eda349b560e538046fbb12314012e546d64dcd1c
hubiqing01 <hubiqing01@58ganji.com>
2017-04-27 16:39:37 +0800

    2.0.2

--- server/routes/helper/upload.js
+++ server/routes/helper/upload.js
@@ -1,8 +1,6 @@
-import promisedFsExtra from 'fs-extra-promise'
-import pathTool from 'path'
-import mkdirp from 'mkdirp-promise'
+const { ioHelper } = appUtils.requireCommon()
 const momentHelper = appUtils.requireCommon('momentHelper')
-//文件上传处理 从临时文件 移动到目标文件夹  文件名加时间字符串前缀
+    //文件上传处理 从临时文件 移动到目标文件夹  文件名加时间字符串前缀
 export let upload = (options) => {
     let defaultOptions = {
         uploadPath: global.APP_CONFIG.uploadPath,
@@ -17,17 +15,17 @@ export let upload = (options) => {
         const year = now.getFullYear()
         const month = now.getMonth() + 1
         const day = now.getDate()
-        //获取要上传到的文件夹
-        let dir = pathTool.isAbsolute(options.dest) ? options.dest : pathTool.join(options.uploadPath, `${year}-${month}-${day}`, options.dest)
-        const exists = await promisedFsExtra.existsAsync(dir)
+            //获取要上传到的文件夹
+        let dir = ioHelper.pathTool.isAbsolute(options.dest) ? options.dest : ioHelper.pathTool.join(options.uploadPath, `${year}-${month}-${day}`, options.dest)
+        const exists = await ioHelper.exists(dir)
         if (!exists) {
-            await mkdirp(dir)
+            await ioHelper.makeDir(dir)
         }
         while (parts.length) {
             let part = parts.shift()
-            //要放置的文件路径 
-            const destFilePath = pathTool.resolve(dir, `${momentHelper.format(null,'YYYYMMDDHHmmss-')}` + part.name)
-            await promisedFsExtra.moveAsync(part.path, destFilePath)
+                //要放置的文件路径 
+            const destFilePath = ioHelper.pathTool.resolve(dir, `${momentHelper.format(null,'YYYYMMDDHHmmss-')}` + part.name)
+            await ioHelper.moveAsync(part.path, destFilePath)
             options.onSave && options.onSave(destFilePath, ctx)
         }
         await next()

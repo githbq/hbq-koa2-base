5e93897e07b4a69c9fb3a0fd2ab47a737e56d8d0
hubiqing01 <hubiqing01@58ganji.com>
2017-04-27 17:32:49 +0800

    2.0.4

--- server/routes/helper/upload.js
+++ server/routes/helper/upload.js
@@ -1,22 +1,16 @@
-const { ioHelper } = appUtils.requireCommon()
-const momentHelper = appUtils.requireCommon('momentHelper')
+const { ioHelper, momentHelper } = appUtils.requireCommon()
     //文件上传处理 从临时文件 移动到目标文件夹  文件名加时间字符串前缀
 export let upload = (options) => {
     let defaultOptions = {
         uploadPath: global.APP_CONFIG.uploadPath,
-        dest: '', //目标路径 相对于path,或者一个绝对路径
-        onSave: null //每一个文件保存的时候 事件
-
+        dest: momentHelper.format(null, 'YYYY-MM-DD'), //目标路径 相对uploadPath  
+        onSave: null //每一个文件保存的时候 事件 
     }
     options = Object.assign({}, defaultOptions, options)
     return async(ctx, next) => {
         let parts = ctx.request.files
-        const now = new Date()
-        const year = now.getFullYear()
-        const month = now.getMonth() + 1
-        const day = now.getDate()
             //获取要上传到的文件夹
-        let dir = ioHelper.pathTool.isAbsolute(options.dest) ? options.dest : ioHelper.pathTool.join(options.uploadPath, `${year}-${month}-${day}`, options.dest)
+        let dir = ioHelper.pathTool.isAbsolute(options.dest) ? options.dest : ioHelper.pathTool.join(options.uploadPath, options.dest)
         const exists = await ioHelper.exists(dir)
         if (!exists) {
             await ioHelper.makeDir(dir)

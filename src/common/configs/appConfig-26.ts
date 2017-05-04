60e3a8b5497070402693b5cf1ccc12cd0c267c80
hubiqing01 <hubiqing01@58ganji.com>
2017-05-04 17:11:42 +0800

    to typescript

--- src/common/configs/appConfig-26.ts
+++ src/common/configs/appConfig-26.ts
@@ -2,5 +2,5 @@ import * as appConfig from './appConfig'
 export default {
     ...appConfig,
     port:4603,
-   mongodb: 'mongodb://doraemonUser:poilkjmnb.zxc@10.9.192.26:27058/doraemon', //远程数据库
+   mongodb: 'mongodb://koa2BaseUser:poilkjmnb.zxc@10.9.192.26:27058/koa2Base', //远程数据库
 }
--- src/common/configs/appConfig.ts
+++ src/common/configs/appConfig.ts
@@ -1,6 +1,6 @@
 import * as path from 'path'
 export default {
-    mongodb: 'mongodb://127.0.0.1:27017/doraemon',
+    mongodb: 'mongodb://127.0.0.1:27017/koa2Base',
     apiPrefix: 'api',
     hostPrefix: '', //应用程序上线域名前缀
     staticPath: path.join(ROOT_PATH, 'public'),
@@ -8,7 +8,7 @@ export default {
     uploadPath: path.join(ROOT_PATH, 'upload'),
     uploadStaticPrefix: 'attachment',
     port: 6666,
-    secret: 'doraemon',
-    projectName: 'doraemonPlatform',
+    secret: 'koa2Base',
+    projectName: 'koa2BasePlatform',
     rootPath: process.cwd()
 }

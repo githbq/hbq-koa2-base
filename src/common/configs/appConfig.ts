17fe61fb425423eeb3c07cc37e8e0e1c704072ef
hubiqing01 <hubiqing01@58ganji.com>
2017-05-16 17:37:12 +0800

    appConfig_update

--- src/common/configs/appConfig.ts
+++ src/common/configs/appConfig.ts
@@ -6,7 +6,7 @@ export default {
     staticPath: pathTool.join(ROOT_PATH, 'public'),
     viewPath: pathTool.join(ROOT_PATH, 'views'),
     uploadPath: pathTool.join(ROOT_PATH, 'upload'),
-    uploadStaticPrefix: 'attachment',
+    uploadStaticPrefix: 'upload',
     port: 6666,
     secret: 'koa2Base',
     projectName: 'koa2Base'

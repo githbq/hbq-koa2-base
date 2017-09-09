ab83079e2eb8232461246b0f38eb022320bb63f5
hbq <hbqhbq123@qq.com>
2017-09-09 23:37:30 +0800

    ok

--- src/common/configs/appConfig.ts
+++ src/common/configs/appConfig.ts
@@ -1,55 +1,55 @@
 import * as pathTool from 'path'
 export default {
-    mongodb: 'mongodb://127.0.0.1:17951/koa2Base',
-    apiPrefix: 'api',
-    hostPrefix: '', //应用程序上线域名前缀
-    staticPath: pathTool.join(ROOT_PATH, 'public'),
-    viewPath: pathTool.join(ROOT_PATH, 'views'),
-    uploadPath: pathTool.join(ROOT_PATH, 'upload'),
-    uploadStaticPrefix: 'upload',
-    port: 6666,
-    secret: 'koa2Base',
-    projectName: 'koa2Base',
-    //创建session的key会产生对应mongodb的集合`${sessionKey}s`
-    sessionKey: 'appSession',
-    staticFtp: {
-        host: 'static.upload.xxdns.org',
-        port: 21,
-        user: 'ubu_wx_web',
-        password: 'gJqLW0o4fJv'
-    },
-    getHostName(): string {
-        return appUtils.isDev() ?
-            `http://localhost:${this.port}` : 'http://xxx.xxx.com'
-    },
-    /**
-     * 获取单点登陆配置
-     */
-    getSsoConfig(): SSOConfig {
-        return appUtils.isDev() ?
-            new SSOConfig(
-                'https://sso.xxx.com:8443/gsso'
-                , '/serviceValidate'
-                , '/logout'
-            )
-            :
-            new SSOConfig(
-                'https://passport.xxx.com'
-                , '/serviceValidate'
-                , '/logout'
-            )
-    }
+  mongodb: 'mongodb://127.0.0.1:17951/koa2Base',
+  apiPrefix: 'api',
+  hostPrefix: '', //应用程序上线域名前缀
+  staticPath: pathTool.join(ROOT_PATH, '..', 'hbq-typescript-react-boilerplate', 'dist'),
+  viewPath: pathTool.join(ROOT_PATH, 'views'),
+  uploadPath: pathTool.join(ROOT_PATH, 'upload'),
+  uploadStaticPrefix: 'upload',
+  port: 6666,
+  secret: 'koa2Base',
+  projectName: 'koa2Base',
+  //创建session的key会产生对应mongodb的集合`${sessionKey}s`
+  sessionKey: 'appSession',
+  staticFtp: {
+    host: 'static.upload.xxdns.org',
+    port: 21,
+    user: 'ubu_wx_web',
+    password: 'gJqLW0o4fJv'
+  },
+  getHostName(): string {
+    return appUtils.isDev() ?
+      `http://localhost:${this.port}` : 'http://xxx.xxx.com'
+  },
+  /**
+   * 获取单点登陆配置
+   */
+  getSsoConfig(): SSOConfig {
+    return appUtils.isDev() ?
+      new SSOConfig(
+        'https://sso.xxx.com:8443/gsso'
+        , '/serviceValidate'
+        , '/logout'
+      )
+      :
+      new SSOConfig(
+        'https://passport.xxx.com'
+        , '/serviceValidate'
+        , '/logout'
+      )
+  }
 }
 
 
 class SSOConfig {
-    public serverPath: string
-    public validate: string
-    public loginOut: string
-    constructor(serverPath: string, validate: string, loginOut: string) {
-        this.serverPath = serverPath
-        this.validate = validate
-        this.loginOut = loginOut
-    }
+  public serverPath: string
+  public validate: string
+  public loginOut: string
+  constructor(serverPath: string, validate: string, loginOut: string) {
+    this.serverPath = serverPath
+    this.validate = validate
+    this.loginOut = loginOut
+  }
 
 }

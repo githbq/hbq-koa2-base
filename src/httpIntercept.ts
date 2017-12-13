d05733382af3f7bdc3ed8a16996bb3e9126dfc54
hubiqing <hubiqing01@58ganji.com>
2017-12-13 16:36:34 +0800

    httpIntercept

--- src/httpIntercept.ts
+++ src/httpIntercept.ts
@@ -5,8 +5,8 @@ const _request = Http.request
 Http.request = function (options, _responseCallback) {
   const responseCallback = function (res) {
     debugger
-    // res.statusCode = 408
-    //res.statusMessage='request timeout'
+    res.statusCode = 408
+    res.statusMessage = 'request timeout'
     return _responseCallback.apply(this, arguments)
   }
   const req = _request.apply(this, [options, responseCallback])

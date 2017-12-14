95a2c90d5cf8e90ebc4f104dbda8b276024445c7
hubiqing <hubiqing01@58ganji.com>
2017-12-14 15:33:29 +0800

    ok

--- src/httpIntercept.ts
+++ src/httpIntercept.ts
@@ -4,10 +4,8 @@ const { EventEmitter } = require('events')
 const _request = Http.request
 Http.request = function (options, _responseCallback) {
   const responseCallback = function (res) {
-    debugger
     res.statusCode = 408
     res.statusMessage = 'request timeout'
-    debugger
     return _responseCallback.apply(this, arguments)
   }
   const req = _request.apply(this, [options, responseCallback])
@@ -16,17 +14,13 @@ Http.request = function (options, _responseCallback) {
   const response = new IncomingMessage(new EventEmitter())
   response.req = req
   req.path = options.path
-  debugger
   setTimeout(() => {
     try {
       req.res = response
       req.emit('response', response)
-      // req.write('hello')
       response.emit('end')
     } catch (e) {
-      debugger
     }
   }, 500)
-  debugger
   return req
 }
